c3519e7a5e15ed746271ae705a034dc7756f0c6a
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-05-28 15:24:17 +0900

    add get_url_for_permission() and store_credentials() and fetch_event_list()

--- back/app/server.py
+++ back/app/server.py
@@ -1,9 +1,30 @@
-from flask import Flask
+from flask import Flask, request
+from calendar_test import Calendar
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return "<h1>Hello, Flask!</h1>"
+
+
+@app.route("/calendar-permission")
+def get_url_for_permission():
+    url = Calendar.get_url_for_permission()
+    return url
+
+@app.route("/oauth2callback")
+def store_credentials():
+    print(request.args.get("code"))
+    Calendar.store_credentials(request.args.get("code"))
+    return "カレンダーの連携完了"
+
+
+@app.route("/calendar-list")
+def fetch_event_list():
+    Calendar.fetch_event_list()
+    return "取得"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
