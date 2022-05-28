1e4aa41cd33662d98cb1769fa601b37543d7d316
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-05-28 15:33:12 +0900

    feat: add calendar-service having credentials

--- back/app/calendarController.py
+++ back/app/calendarController.py
@@ -1,3 +1,8 @@
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+
 class calendarController():
     def getHtml(self):
         return "<h1>test Class</h1>"
@@ -9,11 +14,18 @@ class calendarController():
     @staticmethod
     def getEmptyDates():
         # calendar API
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        service = build('calendar', 'v3', credentials=creds)
+
         empty_dates = ("2022-05-26", "2022-05-27")
         return empty_dates
 
     @staticmethod
     def registerCandidates(company_name, candidate_dates):
+        # calendar API
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        service = build('calendar', 'v3', credentials=creds)
+
         # 候補日を登録
 
         #（日程確認メール送信）
@@ -23,6 +35,10 @@ class calendarController():
 
     @staticmethod
     def registerInterviewDate(company_name, interview_dates):
+        # calendar API
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        service = build('calendar', 'v3', credentials=creds)
+
         #面接日を登録
 
         #（候補日を削除）
