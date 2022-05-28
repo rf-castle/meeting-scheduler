e97fdc8a36abc6c0e8dd5291e49bb11d578d1c50
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-05-28 15:56:08 +0900

    fix: add credentials.json and token.json to .gititnore

---
+++ back/app/calendar_test.py
@@ -0,0 +1,63 @@
+from __future__ import print_function
+import datetime
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+# If modifying these scopes, delete the file token.json.
+
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+PORT = 9000
+REDIRECT_URI = "http://localhost:9000/oauth2callback"
+
+class Calendar:
+
+    def getUrlForPermission():
+
+        flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+        flow.redirect_uri=REDIRECT_URI
+        url_for_permission, _ = flow.authorization_url()
+        return url_for_permission
+
+
+    def storeCredentials(code):
+        
+        flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+        flow.redirect_uri=REDIRECT_URI
+        flow.fetch_token(code=code)
+        creds = flow.credentials
+        with open('token.json', 'w') as token:
+                token.write(creds.to_json())
+        return 
+       
+    def fetchEventList():
+
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        service = build('calendar', 'v3', credentials=creds)
+
+
+        # 予定の取得
+        now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+        print('Getting the upcoming 10 events')
+        events_result = service.events().list(calendarId='primary', timeMin=now,
+                                            maxResults=10, singleEvents=True,
+                                            orderBy='startTime').execute()
+        events = events_result.get('items', [])
+
+        if not events:
+            print('No upcoming events found.')
+        for event in events:
+            # dateTime: 時間指定の予定だったら，ここに入る
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            print(type(start))
+            print(start, event['summary'])
+
+        return 
+
+        
+
+    
---
+++ back/app/server.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, jsonify
+from calendar_test import Calendar
+from calendarController import calendarController
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    test = "testaaaa"
+    return calendarController.testController(test)
+
+# 空いている日程を取得
+@app.route("/empty-dates")
+def getEmptyDates():
+    return jsonify(calendarController.getEmptyDates())
+
+# 候補日を登録
+@app.route("/candidate-dates", methods=['POST'])
+def registerCandidates():
+    company_name = str(request.form.get("company"))
+    candidate_dates = request.form.get("candidate_dates")
+    return jsonify(calendarController.registerCandidates(company_name, candidate_dates))
+
+# 面接日を登録
+@app.route("/interview-date", methods=['POST'])
+def registerInterviewDate():
+    company_name = str(request.form.get("company"))
+    interview_dates = request.form.get("candidate_dates")
+    return jsonify(calendarController.registerInterviewDate(company_name, interview_dates))
+
+
+@app.route("/calendar-permission")
+def getUrlForPermission():
+    url = calendarController.getUrlForPermission()
+    return url
+
+@app.route("/oauth2callback")
+def storeCredentials():
+    print(request.args.get("code"))
+    calendarController.storeCredentials(request.args.get("code"))
+    return "カレンダーの連携完了"
+
+
+@app.route("/calendar-list")
+def fetchEventList():
+    calendarController.fetchEventList()
+    return "取得"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80, debug=True)
